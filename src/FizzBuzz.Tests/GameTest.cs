d5e87bbce43fc7224d1226bc0476ef64b5cb19f5
Pooria <pooria.yoo09@gmail.com>
2022-04-24 11:31:09 +0430

    added the initial project

---
+++ src/FizzBuzz.Tests/GameTest.cs
@@ -0,0 +1,32 @@
+﻿using FizzBuzz.Logic;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FizzBuzz.Tests
+{
+    public class GameTest
+    {
+        private readonly Game _sut;
+
+        public GameTest()
+        {
+            _sut = new Game();
+        }
+
+        [Fact]
+        public void Number_is_devidable_to_3()
+        {
+            var number = 50;
+
+            var expectation = _sut.CheckDividableby3();
+
+            expectation.Should().Be(0);
+        }
+
+    }
+}
